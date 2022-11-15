92ac73b430f88d15e25cee30d4ddb9c8da30c002
nagypetermate <nagypetermate@EOTVOS2.LOCAL>
2022-11-15 15:26:41 +0100

    a weboldal alapját létrehoztam

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <title>@yield('title')</title>
 </head>
 <body>
-    
+    @yield('conent')
 </body>
 </html>
